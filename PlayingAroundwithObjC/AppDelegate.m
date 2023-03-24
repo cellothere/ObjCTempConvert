0ca26eac1cb84339905f0c549e3240a1761bea58
cellothere <109959547+cellothere@users.noreply.github.com>
2023-03-23 21:06:45 -0400

    view model. errors in .m file.

--- PlayingAroundwithObjC/AppDelegate.m
+++ PlayingAroundwithObjC/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -15,7 +16,13 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    self.window = UIWindow.new;
+    [self.window makeKeyWindow];
+    
+    self.window.rootViewController = [[UINavigationController alloc]
+                                      initWithRootViewController:ViewController.new];
+
     return YES;
 }
 
